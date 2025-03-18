199425cc0c26728d809eb4f4ffd8a517e2206225
André Filipe Teixeira de Lima <andrefilipedelima@hotmail.com>
2025-03-18 16:37:33 -0300

    add DepartmentDaoJDBC and DaoFactory create method

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,12 +1,17 @@
 package model.dao;
 
 import db1.DB1;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	
 	public static SellerDao createSellerDao() {
 		return new SellerDaoJDBC(DB1.getConnection());
+	}
+	
+	public static DepartmentDao createDepartmentDao() {
+		return new DepartmentDaoJDBC(DB1.getConnection());
 	}
 
 }
