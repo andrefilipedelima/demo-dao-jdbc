9587aafaf4997055dd0a06b00074ecbc2f10ccad
André Filipe Teixeira de Lima <andrefilipedelima@hotmail.com>
2025-03-18 13:41:33 -0300

    add DepartmentDao and SellerDao interfaces

--- src/model/entities/Seller.java
+++ src/model/entities/Seller.java
@@ -5,18 +5,17 @@ import java.util.Date;
 import java.util.Objects;
 
 public class Seller implements Serializable {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private Integer id;
 	private String name;
 	private String email;
 	private Date birthDate;
 	private Double baseSalary;
 	private Department department;
-	
+
 	private Seller() {
-		
 	}
 
 	public Seller(Integer id, String name, String email, Date birthDate, Double baseSalary, Department department) {
